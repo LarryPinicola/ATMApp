e9f8e26d87d5735b6d9cace1a000c5d9c7b25837
LarryPinicola <thelarry1549@gmail.com>
2024-01-01 23:16:14 +0630

    Add project files.

---
+++ ATMApp/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ATMApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        
+    }
+}
