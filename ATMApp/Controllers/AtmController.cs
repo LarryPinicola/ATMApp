20b462f197d6857d94f245c3a250f88ee063b8d9
LarryPinicola <thelarry1549@gmail.com>
2024-01-01 23:29:41 +0630

    .

--- ATMApp/Controllers/AtmController.cs
+++ ATMApp/Controllers/AtmController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ATMApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ATMApp.Controllers
 {
     public class AtmController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public AtmController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [ActionName("Index")]
+        public IActionResult CardIndex()
         {
             return View();
         }
