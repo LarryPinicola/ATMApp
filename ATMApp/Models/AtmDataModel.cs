c27b44249c52d92f634e4434310b56285996b3cd
LarryPinicola <thelarry1549@gmail.com>
2024-01-02 11:03:25 +0630

    [inProgress] buttons

--- ATMApp/Models/AtmDataModel.cs
+++ ATMApp/Models/AtmDataModel.cs
@@ -7,11 +7,17 @@ namespace ATMApp.Models
     public class AtmDataModel
     {
         [Key]
+        [Column("card_Id")]
         public int CardId { get; set; }
+        [Column("cardNum")]
         public double CardNum { get; set; }
+        [Column("card_pin")]
         public int CardPin { get; set; }
+        [Column("card_firstName")]
         public string FirstName { get; set; }
+        [Column("card_lastName")]
         public string LastName { get; set; }
+        [Column("card_balance")]
         public double Balance { get; set; }
     }
 }
